929745bdb28bc9ff462b8b4793cf34cedb7207e8
fong <isfong@qq.com>
2019-05-28 18:59:38 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,11 +1,19 @@
-import Vue   from 'vue'
-import App   from './App'
-import store from "./store";
-import http  from "./http";
+import Vue        from 'vue'
+import App        from './App'
+import store      from "./store";
+import http       from "./http";
+import components from './pages/component/home.vue'
+import plugin     from './pages/plugin/home.vue'
+import basics     from './pages/basics/home.vue'
+import cuCustom   from './components/colorui/cu-custom.vue'
 
 Vue.config.productionTip = false;
 Vue.prototype.$store = store;
 Vue.prototype.$http = http;
+Vue.component( 'basics', basics );
+Vue.component( 'components', components );
+Vue.component( 'plugin', plugin );
+Vue.component( 'cu-custom', cuCustom );
 
 App.mpType = 'app';
 
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,8 +1,27 @@
 <template>
-    <view class="content">
-        <image class="logo" src="/static/logo.png"></image>
-        <view>
-            <text class="title">{{title}}</text>
+    <view>
+        <basics v-if="PageCur==='basics'"></basics>
+        <components v-if="PageCur==='component'"></components>
+        <plugin v-if="PageCur==='plugin'"></plugin>
+        <view class="cu-bar tabbar bg-white shadow foot">
+            <view class="action" @click="NavChange" data-cur="basics">
+                <view class='cuIcon-cu-image'>
+                    <image :src="'/static/img/colorui/tabbar/basics' + [PageCur==='basics'?'_cur':''] + '.png'"></image>
+                </view>
+                <view :class="PageCur==='basics'?'text-green':'text-gray'">元素</view>
+            </view>
+            <view class="action" @click="NavChange" data-cur="component">
+                <view class='cuIcon-cu-image'>
+                    <image :src="'/static/img/colorui/tabbar/component' + [PageCur === 'component'?'_cur':''] + '.png'"></image>
+                </view>
+                <view :class="PageCur==='component'?'text-green':'text-gray'">组件</view>
+            </view>
+            <view class="action" @click="NavChange" data-cur="plugin">
+                <view class='cuIcon-cu-image'>
+                    <image :src="'/static/img/colorui/tabbar/plugin' + [PageCur === 'plugin'?'_cur':''] + '.png'"></image>
+                </view>
+                <view :class="PageCur==='plugin'?'text-green':'text-gray'">扩展</view>
+            </view>
         </view>
     </view>
 </template>
@@ -11,30 +30,16 @@
     export default {
         data() {
             return {
-                title: 'Hello'
+                PageCur: 'basics'
             }
         },
-        onLoad() {
-
-        },
-        methods: {}
+        methods: {
+            NavChange: function ( e ) {
+                this.PageCur = e.currentTarget.dataset.cur
+            }
+        }
     }
 </script>
 
 <style>
-    .content {
-        text-align: center;
-        height: 400upx;
-    }
-
-    .logo {
-        height: 200upx;
-        width: 200upx;
-        margin-top: 200upx;
-    }
-
-    .title {
-        font-size: 36upx;
-        color: #8f8f94;
-    }
 </style>
