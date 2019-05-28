e12da76260d7cfb69b894f16f7614543f529ee02
fong <isfong@qq.com>
2019-05-28 18:44:39 +0800

    init

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,12 @@
-const pkg = require('./package.json')
+const pkg = require( './package.json' );
 module.exports = {
-  parser: require('postcss-comment'),
-  plugins: [
-    require('postcss-import'),
-    require('autoprefixer')({
-      browsers: pkg.browserslist,
-      remove: process.env.UNI_PLATFORM !== 'h5'
-    }),
-    require('@dcloudio/vue-cli-plugin-uni/packages/postcss')
-  ]
-}
+    parser: require( 'postcss-comment' ),
+    plugins: [
+        require( 'postcss-import' ),
+        require( 'autoprefixer' )( {
+            browsers: pkg.browserslist,
+            remove: process.env.UNI_PLATFORM !== 'h5'
+        } ),
+        require( '@dcloudio/vue-cli-plugin-uni/packages/postcss' )
+    ]
+};
